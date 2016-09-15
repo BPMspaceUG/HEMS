08f58e7f61152f00afa031e5a27cc5ba35986fac
Cobkirchner <christian.obkirchner@bpmspace.com>
2016-09-15 16:30:34 +0200

    Update

--- _Kali_Template/download_vm-autoconfiguration_kali.sh
+++ _Kali_Template/download_vm-autoconfiguration_kali.sh
@@ -1,5 +1,5 @@
 # Download and update the vm-autoconfiguration file in /etc/init.d/vm-autoconfiguration.sh
 
-wget -N --output-document=/etc/init.d/vm-autoconfiguration.sh  https://raw.githubusercontent.com/BPMspaceUG/HEMS/master/_Kali_Template/vm-autoconfiguration_kali.sh
+wget --no-use-server-timestamps --output-document=/etc/init.d/vm-autoconfiguration.sh  https://raw.githubusercontent.com/BPMspaceUG/HEMS/master/_Kali_Template/vm-autoconfiguration_kali.sh
 
-sudo sh /etc/init.d/vm-autoconfiguration.sh
+sh /etc/init.d/vm-autoconfiguration.sh
