2891b52c5e02d92d176e94288f49ba4d34d51505
Cobkirchner <christian.obkirchner@bpmspace.com>
2016-09-15 15:55:00 +0200

    update download_vm_autoconfig

--- _Metasploitable_Template/download_vm-autoconfiguration_metasploitable.sh
+++ _Metasploitable_Template/download_vm-autoconfiguration_metasploitable.sh
@@ -1,5 +1,5 @@
 # Download and update the vm-autoconfiguration file in /etc/init.d/vm-autoconfiguration.sh
 
-wget --no-check-certificate --output-document=/etc/init.d/vm-autoconfiguration.sh  https://raw.githubusercontent.com/BPMspaceUG/HEMS/master/_Metasploitable_Template/vm-autoconfiguration_metasploitable.sh
+wget -N --no-check-certificate --output-document=/etc/init.d/download_vm-autoconfiguration.sh  https://raw.githubusercontent.com/BPMspaceUG/HEMS/master/_Metasploitable_Template/download_vm-autoconfiguration_metasploitable.sh
 
 sudo sh /etc/init.d/vm-autoconfiguration.sh
