9f30bf83188747c0f9490c595308ebe489601e23
Cobkirchner <christian.obkirchner@bpmspace.com>
2016-09-20 13:45:55 +0200

    added autochangeing of hosts, hostname and netwrok/interfaces

--- _Kali_Template/vm-autoconfiguration_kali.sh
+++ _Kali_Template/vm-autoconfiguration_kali.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #stored as /etc/init.d/vm-autoconfiguration.sh
-#last modified: 20.09.2016 - 11:29
+#last modified: 20.09.2016 - 13:43
 
 # gets the last two characters of the mac-address and stores it in the variable "mac"
 mac="`ip link show dev eth0 | grep -oE 'link/ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut -d' ' -f2 |tail -c3 `" 
@@ -13,12 +13,24 @@ dec_two_digit="$(printf '%02d' "$mac_decimal")"
 vm_type=1 
 # sets the static ip address according to the MAC-Address
 static_ip="$mac_decimal""$vm_type"
-ifconfig eth0 10.42.42.$static_ip netmask 255.255.255.0 up
+#ifconfig eth0 10.42.42.$static_ip netmask 255.255.255.0 up
 # sets the default gateway to create an internet connection via the hems_router
-route add default gw 10.42.42.254
+#route add default gw 10.42.42.254
+
+#edits the file /etc/network/interfaces
+sed -i '/inet dhcp/d' /etc/network/interfaces #deletes the line where eth0 is set to dhcp
+# sets eth0 to static configuration
+echo "iface eth0 inet static" >> /etc/network/interfaces
+echo "address 10.42.42.$static_ip">> /etc/network/interfaces
+echo "netmask 255.255.255.0" >> /etc/network/interfaces
+echo "gateway 10.42.42.254" >> /etc/network/interfaces
+ifdown eth0
+ifup eth0
+/etc/init.d/networking restart
 
 #Changes the hostname in /etc/hosts and /etc/hostname
 hostname "kali-lab""$dec_two_digit"
 echo "kali-lab""$dec_two_digit" > /etc/hostname
-echo "127.0.0.1  localhost"\n"127.0.1.1  kali-lab""$dec_two_digit" > /etc/hosts
+sed -i 2D /etc/hosts
+echo "127.0.1.1  kali-lab""$dec_two_digit" >> /etc/hosts    
 
