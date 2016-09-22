1885b4f843357d68b458c1316f8720e3331153df
Cobkirchner <christian.obkirchner@bpmspace.com>
2016-09-22 12:10:31 +0200

    changed ip config and hostname changing method

--- _Kali_Template/vm-autoconfiguration_kali.sh
+++ _Kali_Template/vm-autoconfiguration_kali.sh
@@ -18,27 +18,46 @@ vm_type=1
 #route add default gw 10.42.42.254
 
 #edits the file /etc/network/interfaces
-sed -i '/inet dhcp/d' /etc/network/interfaces #deletes the line where eth0 is set to dhcp
-sed -i '/iface eth0 inet static/d' /etc/network/interfaces
-sed -i '/address/d' /etc/network/interfaces #deletes old entries for adress, network and gateway 
-sed -i '/broadcast/d' /etc/network/interfaces
-sed -i '/netmask/d' /etc/network/interfaces
-sed -i '/gateway/d' /etc/network/interfaces
-# sets eth0 to static configuration
-echo "iface eth0 inet static" >> /etc/network/interfaces
-echo "address 10.42.$mac_decimal.1">> /etc/network/interfaces
-echo "broadcast 10.42.42.254" >> /etc/network/interfaces
-echo "netmask 255.255.255.0" >> /etc/network/interfaces
-echo "gateway 10.42.42.254" >> /etc/network/interfaces
-sleep 2
-/etc/init.d/networking restart
+current_ipaddress="`sed -n '12 p' /etc/network/interfaces | cut -d ' ' -f2`"
+estimated_ipaddress="10.42.$mac_decimal.1"
+if [$current_ipaddress != "$estimated_ipaddress"]
+then
+	sed -i '/inet dhcp/d' /etc/network/interfaces #deletes the line where eth0 is set to dhcp
+	sed -i '/iface eth0 inet static/d' /etc/network/interfaces
+	sed -i '/address/d' /etc/network/interfaces #deletes old entries for address, network and gateway 
+	sed -i '/broadcast/d' /etc/network/interfaces
+	sed -i '/netmask/d' /etc/network/interfaces
+	sed -i '/gateway/d' /etc/network/interfaces
+	# sets the new static  ip configuration for eth0
+	echo "iface eth0 inet static" >> /etc/network/interfaces
+	echo "address 10.42.$mac_decimal.1">> /etc/network/interfaces
+	#echo "broadcast 10.42.254.254" >> /etc/network/interfaces
+	echo "netmask 255.255.0.0" >> /etc/network/interfaces
+	echo "gateway 10.42.254.254" >> /etc/network/interfaces
+
+	sleep 1
+	sudo /etc/init.d/networking restart
+	sleep 1
+else 
+	echo "$current_ipaddress"
+
+fi
+
 
 #Changes the hostname in /etc/hosts and /etc/hostname
-hostname "kali-lab""$dec_two_digit"
-echo "kali-lab""$dec_two_digit" > /etc/hostname
-sed -i 2D /etc/hosts
-echo "127.0.1.1  kali-lab""$dec_two_digit" >> /etc/hosts    
-sleep 2
-# restarts the ssh service with the new settings
-service sshd restart
+current_hostname="`cat /etc/hostname`"
+estimated_hostname="kali-lab$dec_two_digit"
+if ["$current_hostname" != "$estimated_hostname" ]
+then
+	#hostname "kali-lab""$dec_two_digit"
+	echo "kali-lab""$dec_two_digit" > /etc/hostname
+	sed -i 2D /etc/hosts
+	echo "127.0.1.1  kali-lab""$dec_two_digit" >> /etc/hosts    
+	sleep 1
+	# restarts the ssh service with the new settings
+	service sshd restart
+else 
+	echo $current_hostname
+fi
 
+sudo reboot
