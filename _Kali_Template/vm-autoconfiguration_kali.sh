ce3dbcb3211b84377c3f6ab235daeac67af5249d
Cobkirchner <christian.obkirchner@bpmspace.com>
2016-09-22 16:35:33 +0200

    echo updated

--- _Kali_Template/vm-autoconfiguration_kali.sh
+++ _Kali_Template/vm-autoconfiguration_kali.sh
@@ -66,7 +66,8 @@ else
 	#sed -i '/127.0.1.1/d' /etc/hosts
 	sleep 1
 	#echo "127.0.1.1  kali-lab""$dec_two_digit" >> /etc/hosts
-	echo -e "127.0.0.1 localhost\n127.0.1.1 kali-lab$dec_two_digit" > /etc/hosts
+	echo "127.0.0.1 localhost"  > /etc/hosts
+	echo "127.0.1.1 kali-lab$dec_two_digit" >> /etc/hosts
 	sudo chmod 644 /etc/hosts
 	sudo chmod 644 /etc/hostname    
 	sleep 1
