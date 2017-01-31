75dac057935113568b2312278955fe1bdc7520ba
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-01-31 16:06:59 +0100

    update vnc

--- _Kali_Template/vm-autoconfiguration_kali.sh
+++ _Kali_Template/vm-autoconfiguration_kali.sh
@@ -92,10 +92,8 @@ sudo /opt/nessus/sbin/nessuscli fetch --register-offline /opt/nessus/etc/nessus/
 sudo /etc/init.d/nessusd start
 sleep 5
 
-#Configure XRDP and Tightvnc
-sudo service xrdp stop
+#Start Tightvnc
 vncserver :1 -geometry 1920x1080 -depth 16 -localhost
-sudo service xrdp start
 
 #EOF
 
